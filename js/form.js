2a7012a4365f15a286d3d8ce400c255277b85919
VINICIUS CORREIA <viniciuscorreia.couto@gmail.com>
2025-03-31 15:31:25 -0300

    contato

--- js/form.js
+++ js/form.js
@@ -3,24 +3,22 @@
 
 class contato {
     ////Exportar a lista do html para o JS
-    constructor (nome, sobrenome, email, cpf, telefone, contato) {
+    constructor (nome, email, telefone, contato ,mensagem) {
         this.nome = nome;
-        this.sobrenome = sobrenome;
         this.email = email;
-        this.cpf = cpf;
         this.telefone = telefone;
         this.contato = contato;
+        this.mensagem = mensagem
     }
 }
 //função para enviar os dados
 function Post(form) {
     //variavel para armazenar os dados
   let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
             form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
+            form.elements.namedItem("contato").value,
+            form.elements.namedItem("mensagem").value);
             console.table(data);
   Enviar(data);
 }
@@ -29,6 +27,4 @@ function Enviar(data) {
     if (data.nome != "") {
         alert('Obrigado sr(a) ' + data.nome + ' os seus dados foram encaminhados com sucesso');
     }
-    
-    
 }
